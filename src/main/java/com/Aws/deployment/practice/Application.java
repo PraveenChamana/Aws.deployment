ada3c801553d465219496d45c88e9d7e31fe9fb5
praveen <praveenchamana@gmail.com>
2021-12-20 23:04:11 +0530

    initial commit

---
+++ src/main/java/com/Aws/deployment/practice/Application.java
@@ -0,0 +1,13 @@
+package com.Aws.deployment.practice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Application.class, args);
+	}
+
+}
---
+++ src/main/java/com/Aws/deployment/practice/RestApi.java
@@ -0,0 +1,14 @@
+package com.Aws.deployment.practice;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+@RestController
+public class RestApi {
+	
+	@RequestMapping("/deply")
+	public String m1() {
+		
+		
+		return "Deployment";
+	}
+
+}
